5e371f4981474164e1ad017b190bd4b102c2da40
Jamal RahimZadegan <52101670+jamal-rahimzadegan@users.noreply.github.com>
2021-09-20 19:18:10 +0430

    Added Map

---
+++ src/map/MapStyle.tsx
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+import { MapContainer } from 'react-leaflet';
+
+export const MapStyle = styled(MapContainer)`
+  //border: 2px solid ${({ theme }) => theme.DIVIDER};
+  //border-radius: 10px;
+  height: 100%;
+  width: 100%;
+`;
