f08aba59045f04ff7c8badf1ac805f87e2150c91
Jamal RahimZadegan <52101670+jamal-rahimzadegan@users.noreply.github.com>
2021-10-22 13:11:33 +0330

    Update DropDownStyle.tsx

--- src/DropDown/DropDownStyle.tsx
+++ src/DropDown/DropDownStyle.tsx
@@ -1,11 +1,50 @@
-import styled from 'styled-components';
-import { Card } from '../index';
+import styled, { keyframes } from 'styled-components';
+import { Card } from '../../';
+import { RADIUS } from 'constant';
 
-export const StyledDropDownItemsWrapper = styled(Card)`
+const openDrawer = keyframes`
+  from {
+    height: 0;
+  }
+  to {
+    height: 200px;
+  }`;
+
+const closeDrawer = keyframes`
+  from {
+    height: 200px;
+  }
+  to {
+    height: 0;
+  }
+`;
+
+export const StyledDropDown = styled(Card)`
+  button {
+    border-radius: ${({ isOpen }) => {
+      const { m } = RADIUS;
+      return isOpen ? `${m} ${m} 0 0` : null;
+    }};
+  }
+`;
+
+export const ItemsContainer = styled(Card)`
   height: 0;
-  left: 50%;
-  width: ${(props) => props.width ?? 'max-content'};
-  transform: translate(-50%, 0);
-  box-shadow: 0 2px 5px 0 rgba(0, 0, 0, 0.3);
+  width: inherit;
   z-index: 2;
+  position: absolute;
+  overflow-y: auto;
+  overflow-x: hidden;
+  background: ${({ theme }) => theme.LIGHT_BLOCK_BG};
+  border-radius: ${() => {
+    const { m } = RADIUS;
+    return `0 0 ${m} ${m}`;
+  }};
+
+  animation: ${({ isOpen }) => {
+      if (isOpen === undefined || isOpen === null) return;
+      if (isOpen) return openDrawer;
+      if (!isOpen) return closeDrawer;
+    }}
+    200ms forwards;
 `;
