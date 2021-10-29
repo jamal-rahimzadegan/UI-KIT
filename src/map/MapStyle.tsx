105c5b63ed3b2ae3054b326efc0aa8053cb2f3aa
Jamal RahimZadegan <52101670+jamal-rahimzadegan@users.noreply.github.com>
2021-10-30 00:11:36 +0330

    Delete src/map directory

--- src/map/MapStyle.tsx
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components';
-import { MapContainer } from 'react-leaflet';
-
-export const MapStyle = styled(MapContainer)`
-  //border: 2px solid ${({ theme }) => theme.DIVIDER};
-  //border-radius: 10px;
-  height: 100%;
-  width: 100%;
-`;
