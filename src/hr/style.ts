015716b3cd480214c05542adfb60c654326454cb
Jamal <jamalrahimzadegan@gmail.com>
2023-02-05 23:13:32 +0300

    Update and rename src/Hr/HrStyle.tsx to src/hr/style.ts

--- src/hr/style.ts
+++ src/hr/style.ts
@@ -1,7 +1,7 @@
-import styled from 'styled-components';
-
-export const HrStyle = styled.hr`
+ const Container = styled.hr`
   height: ${(props) => props?.height || '1.5px'};
   width: 100%;
   background-color: ${({ theme, bgColor }) => theme[bgColor || 'DIVIDER']};
 `;
+
+export Container
