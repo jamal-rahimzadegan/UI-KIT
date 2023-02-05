eac0a2883d4373a22677f625076a729d24037883
Jamal <jamalrahimzadegan@gmail.com>
2023-02-05 23:01:18 +0300

    Update next-js-breadcrumb.tsx

--- src/next-js-breadcrumb.tsx
+++ src/next-js-breadcrumb.tsx
@@ -1,52 +1,54 @@
+import {useEffect, useState} from "react";
+
 type BreadCrumbItemsType = { title: string; href: string }[];
 
 export default function Breadcrumb(): JSX.Element {
-  const router = useRouter();
-  const [breadcrumbs, setBreadcrumbs] = useState<BreadCrumbItemsType>([]);
+    const router = useRouter();
+    const [breadcrumbs, setBreadcrumbs] = useState<BreadCrumbItemsType>([]);
 
-  const handleBreadCrumb = () => {
-    if (router) {
-      let splicedPath = router.asPath.split('/');
-      splicedPath.shift();
+    const handleBreadCrumb = () => {
+        if (!router) return
 
-      let pathList = splicedPath.map((path, i) => {
-        return {
-          title: document.title,
-          href: '/' + splicedPath.slice(0, i + 1).join('/'),
-        };
-      });
+        const splicedPath = router.asPath.split('/');
+        splicedPath.shift();
+        const pathList = splicedPath.map((path: string, i: number) => {
+            return {
+                title: document.title,
+                href: '/' + splicedPath.slice(0, i + 1).join('/'),
+            };
+        });
 
-      setBreadcrumbs(pathList);
-    }
-  };
+        setBreadcrumbs(pathList);
+    };
 
-  const handlePathName = (title) => {
-    const path = title.replaceAll('/', '');
-    switch (path) {
-      case 'static name of page':
-        return 'it is a correspond to static title';
-      default:
-        return '';
-    }
-  };
+    const handlePathName = (title: string): string => {
+        const path = title.replaceAll('/', '');
 
-  const navigate = (route) => router.push(route);
+        switch (path) {
+            case 'static name of page':
+                return 'it is a correspond to static title';
+            default:
+                return '';
+        }
+    };
 
-  useEffect(handleBreadCrumb , [router]);
+    const navigate = (route) => router.push(route);
 
-  return breadcrumbs ? (
-    <div className="d-flex align-items-center">
-      <p onClick={() => navigate('/')}>home</p>
-      {breadcrumbs.map((item, i) => {
-        return item.title !== 'homepage title goes here' ? ( // exclude your homepage if you have a button that goes to home page by this condition
-          <>
-            <p className="mx-1">-</p>
-            <p onClick={() => navigate(item.href)} key={item.href}>
-              {i === breadcrumbs.length - 1 ? item.title : handlePathName(item.href)}
-            </p>
-          </>
-        ) : null;
-      })}
-    </div>
-  ) : null;
+    useEffect(handleBreadCrumb, [router]);
+
+    return <>{
+        breadcrumbs ?
+            <>
+                <p onClick={() => navigate('/')}>home</p>
+                {breadcrumbs.map((item, i) =>
+                    item.title !== 'homepage title goes here' ? ( // exclude your homepage if you have a button that goes to home page by this condition
+                        <>
+                            <p>-</p>
+                            <button onClick={() => navigate(item.href)} key={item.href}>
+                                {i === breadcrumbs.length - 1 ? item.title : handlePathName(item.href)}
+                            </button>
+                        </>
+                    ) : null)}
+            </> : null
+    }</>
 }
