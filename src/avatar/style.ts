b3113c4a8d9cea2f1ab4602f6a9c21e27a475613
Jamal <jamalrahimzadegan@gmail.com>
2023-02-05 23:07:24 +0300

    Update style.ts

--- src/avatar/style.ts
+++ src/avatar/style.ts
@@ -1,15 +1,15 @@
 import styled from "styled-components";
 
-const AvatarContainer = styled.div<{ shouldShowTitle: boolean }>`
-	height: 40px;
-	width: 40px;
-	border-radius: 40px;
-	display: flex;
-	align-items: center;
-	justify-content: center;
-	overflow: hidden;
-	box-shadow: 0 0 1rem rgba(0, 0, 0, 0.1);
-	background: ${(props) => (props.shouldShowTitle ? "white" : "transparent")};
+const AvatarContainer = styled.div<{ hasImg: boolean }>`
+  height: 40px;
+  width: 40px;
+  border-radius: 40px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  overflow: hidden;
+  box-shadow: 0 0 1rem rgba(0, 0, 0, 0.1);
+  background: ${({hasImg}) => (hasImg ? "white" : "transparent")};
 `;
 
 const UserNameTitle = styled.div`
