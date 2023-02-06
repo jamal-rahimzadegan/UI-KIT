19b51680c063bb2784741ef1fff7a5c867cc646e
Jamal <jamalrahimzadegan@gmail.com>
2023-02-06 10:45:22 +0300

    Update list.tsx

--- src/list/list.tsx
+++ src/list/list.tsx
@@ -1,35 +1,38 @@
-
-import React, { ReactChildren, ReactNode } from 'react'
+import React, {ReactNode} from 'react'
+import Container from "./style";
 
 interface Props<T = any[]> {
-  testId?: string
-  isLoading?: boolean
-  isEmpty?: boolean
-  emptyMsg?: string
-  error?: boolean
-  className?: string
-  data: T
-  children?: Element | ReactNode | JSX.Element | ReactChildren
+    testId?: string
+    isLoading?: boolean
+    isEmpty?: boolean
+    emptyMsg?: string
+    error?: boolean
+    className?: string
+    data: T
+    as?: "ol" | "ul" | "dd"
+    children?: Element | ReactNode | JSX.Element
 }
 
 type ContentProps = Omit<Props, 'testId' | 'className'>
 
-const RenderContent = (props: ContentProps): JSX.Element => {
-  const { children, data, isLoading = true, error, emptyMsg, isEmpty } = props
-
-  if (isLoading) return <p>Loading...</p>
-  else if (data.length) return <>{children}</>
-  else if (isEmpty) return <p>{emptyMsg}</p>
-  else if (error) return <p>Something Went Wrong</p>
-  else return <></>
-}
 
 export default function List(props: Props): JSX.Element {
-  const { testId, className = '', ...restProps } = props
+    const {testId, as, className = '', ...restProps} = props
 
-  return (
-    <div data-testid={testId} className={className}>
-      {<RenderContent {...restProps} />}
-    </div>
-  )
+    return (
+        <Container as={as} data-testid={testId} className={className}>
+            {<RenderContent {...restProps} />}
+        </Container>
+    )
+}
+
+
+const RenderContent = (props: ContentProps): JSX.Element => {
+    const {children, data, isLoading = true, error, emptyMsg, isEmpty} = props
+
+    if (isLoading) return <p>Loading...</p>
+    else if (data.length) return <>{children}</>
+    else if (isEmpty) return <p>{emptyMsg}</p>
+    else if (error) return <p>Something Went Wrong</p>
+    else return <></>
 }
