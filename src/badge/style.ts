d825211fc79e800dc47a3c44e29c1dd432228ca9
Jamal <jamalrahimzadegan@gmail.com>
2023-02-07 12:11:39 +0300

    Create style.ts

---
+++ src/badge/style.ts
@@ -0,0 +1,21 @@
+import styled from "styled-components";
+
+const Wrapper = styled.section`
+  display: flex;
+  align-items: center;
+  
+  .badge-count {
+    font-weight: bold;
+    color: white;
+    width: 30px;
+    height: 30px;
+    border-radius: 30px;
+    background: #292983;
+    box-shadow: 3px 5px 5px 0 rgba(0, 0, 0, 0.12);
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+`;
+
+export default Wrapper
