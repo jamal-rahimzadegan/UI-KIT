b3c7c8bb3b0fe8cb95949d4b5a5791b68bce17cf
Jamal <jamalrahimzadegan@gmail.com>
2023-02-07 12:11:16 +0300

    Create badge.tsx

---
+++ src/badge/badge.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Wrapper from './style';
+
+interface Props {
+    count: number
+    children?: any
+}
+
+export default function Badge(props: Props): JSX.Element {
+    const {count, children} = props
+    return <Wrapper>{children}<p className="badge-count">{count}</p></Wrapper>
+}
