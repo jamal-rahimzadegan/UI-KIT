0d3832413e462f1135a9aa3ace5099161c27a49f
Jamal <jamalrahimzadegan@gmail.com>
2023-03-08 09:32:44 +0000

    Update and rename src/Video/VideoStyle.tsx to src/video/style.ts

--- src/Video/VideoStyle.tsx
+++
@@ -1,7 +0,0 @@
-import styled from 'styled-components';
-
-export const StyledVideo = styled.video`
-  object-fit: ${(props) => props.objectFit};
-  height: ${(props) => props.height};
-  width: ${(props) => props.width};
-`;
