c4d862cf4c097b32fbc48395ccec86e8391d5379
Jamal <jamalrahimzadegan@gmail.com>
2023-03-21 09:58:37 +0000

    Update and rename ImgStyle.tsx to style.tsx

--- src/IMG/ImgStyle.tsx
+++
@@ -1,8 +0,0 @@
-import styled from 'styled-components';
-import { BORDER_RADIUS } from 'constant';
-
-export const ImgStyle = styled.img`
-  transition: display 1s linear;
-  object-fit: ${({ objectFit }: { objectFit: string }) => objectFit};
-  border-radius: ${({ radius }: { radius: string }) => BORDER_RADIUS[radius]};
-`;
