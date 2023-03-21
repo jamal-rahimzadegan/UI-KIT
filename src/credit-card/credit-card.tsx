58d7f784c80b35b9c4f583bf776ded893c90fc20
Jamal <jamalrahimzadegan@gmail.com>
2023-03-21 11:50:14 +0000

    Feat: add credit-card component

---
+++ src/credit-card/credit-card.tsx
@@ -0,0 +1,29 @@
+import Card from './style'
+import ChipImg from './data/chip.png'
+import formatCardNumber from './data/format-card-number' // You can use any file you prefer
+
+interface Props {
+  bankName: string
+  cardNumber: string
+  holder: string
+  expiry: string
+}
+
+export default function CreditCard(props: Props): JSX.Element {
+  const { bankName, cardNumber, holder, expiry } = props
+
+  // TODO: add validation for card etc.
+  return (
+    <Card.Wrapper>
+      <Card.Header>
+        <Card.BankName>{bankName}</Card.BankName>
+        <Card.ChipIcon src={ChipImg} alt='chip' />
+      </Card.Header>
+      <Card.Number>{formatCardNumber(cardNumber)}</Card.Number>
+      <Card.HolderInfo>
+        <Card.Holder>{holder}</Card.Holder>
+        <Card.ExpDate>{expiry}</Card.ExpDate>
+      </Card.HolderInfo>
+    </Card.Wrapper>
+  )
+}
---
+++ src/credit-card/style.ts
@@ -0,0 +1,60 @@
+import styled from 'styled-components'
+
+const Wrapper = styled.div`
+  box-shadow: 0 0 5px 0 rgba(0, 0, 0, 0.6);
+  background: lightgrey;
+  width: 300px;
+  height: 180px;
+  border-radius: 5px;
+  padding: 0 15px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+`
+
+const Header = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`
+
+const ChipIcon = styled.img`
+  height: 30px;
+`
+
+const BankName = styled.p`
+  font-size: 1.3rem;
+  font-weight: bold;
+`
+
+const Number = styled.p`
+  font-size: 1.1rem;
+`
+
+const HolderInfo = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`
+
+const Holder = styled.p`
+  text-overflow: ellipsis;
+  white-space: nowrap;
+  overflow: hidden;
+  width: 75%;
+`
+
+const ExpDate = styled.p`
+  font-weight: bold;
+`
+
+export default {
+  Wrapper,
+  Header,
+  ChipIcon,
+  BankName,
+  Number,
+  HolderInfo,
+  Holder,
+  ExpDate,
+}
