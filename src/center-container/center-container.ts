7f6ec0146af854b2180b29627ce942268b5f4135
Jamal <jamalrahimzadegan@gmail.com>
2023-03-28 11:07:36 +0100

    Update: center-container style

---
+++ src/center-container/center-container.ts
@@ -0,0 +1,11 @@
+import styled from 'styled-components'
+
+const Wrapper = styled.div`
+  height: 100vh;
+  overflow: hidden;
+  position: relative;
+  max-width: 960px;
+  margin: 0 auto;
+`
+
+export default Wrapper
