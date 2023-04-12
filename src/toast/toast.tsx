9f7b57435701fdb3d56b25325e2931875c48ebcd
Jamal <jamalrahimzadegan@gmail.com>
2023-04-12 17:29:47 +0100

    Update toast.tsx

--- src/toast/toast.tsx
+++ src/toast/toast.tsx
@@ -1,50 +1,70 @@
-import { TOAST_SET } from "./constant";
-import "./styles.css";
-
-// FIXME:  1- Close ,  2- mulitple
+import { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import uiSlice from '@src/store/slices/ui-slice'
+import { TOAST_SET } from './constant'
+import './style.css'
 
 interface Props {
-  visible: boolean;
-  type: "success" | "error" | "info" | "warn";
-  position: "top-right" | "bottom-right" | "top-center" | "bottom-center";
-  title?: string;
-  desc?: string;
+  type?: 'success' | 'error' | 'info' | 'warn'
+  position?: 'top-right' | 'bottom-right' | 'top-center' | 'bottom-center'
+  title?: string
+  body?: string
 }
 
 export type ToastInfo = {
-  bgColor: string;
-  icon: string;
-  title: string;
-  desc: string;
-};
+  bgColor: string
+  icon: string
+  title: string
+  desc: string
+}
 
-export type ToastSet = Record<Props["type"], ToastInfo>;
+declare global {
+  interface Window {
+    toast: {
+      show: Function
+      hide: Function
+    }
+  }
+}
 
-export function Toast(props: Props) {
-  const { visible, type, position } = props;
-  const toast: ToastInfo = TOAST_SET[type];
+export type ToastSet = Record<Props['type'], ToastInfo>
 
-  const closeToast = () => {
-    console.log("close me");
-  };
+export default function Toast(props: Props) {
+  const { type = 'error', position } = props
+  const toast: ToastInfo = TOAST_SET[type]
+  const { isToastVisible = true } = useSelector((state: RootState) => state.ui)
+  const dispatch = useDispatch()
+  const timeout: number = 3000
 
-  if (!visible) return <></>;
+  const makeToastGlobal = () => {
+    ;(window as any).toast = {
+      show: () => dispatch(uiSlice.showToast(true)),
+      hide: () => dispatch(uiSlice.showToast(false)),
+    }
+  }
+
+  const closeToast = () => dispatch(uiSlice.showToast(false))
+
+  useEffect(() => {
+    if (isToastVisible) setTimeout(closeToast, timeout)
+  }, [isToastVisible])
+
+  useEffect(makeToastGlobal, [])
+
+  if (!isToastVisible) return <></>
 
   return (
     <div className={`notification-container ${position}`}>
       <div
         style={{ background: toast.bgColor }}
-        className={`notification toast ${position}`}
+        className={`notification-content ${position}`}
       >
-        <button onClick={closeToast}>×</button>
-        <section className="notification-image">
-          <img src={toast.icon} alt={type + " toast icon"} />
-        </section>
         <section>
-          <p className="notification-title">{toast.title}</p>
-          <p className="notification-message">{toast.desc}</p>
+          <p className='notification-title'>{toast.title}</p>
+          <p className='notification-message'>{toast.desc}</p>
         </section>
+        <button onClick={closeToast}>×</button>
       </div>
     </div>
-  );
+  )
 }
