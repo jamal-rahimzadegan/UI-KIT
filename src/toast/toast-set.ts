aa3ba2be47acd1893d6b6224a7767308b432aeb5
Jamal <jamalrahimzadegan@gmail.com>
2023-04-13 10:16:40 +0100

    Update and rename constant.ts to toast-set.ts

---
+++ src/toast/toast-set.ts
@@ -0,0 +1,26 @@
+export const TOAST_SET: ToastSet = {
+  success: {
+    bgColor: 'green',
+    icon: '',
+    title: 'Success',
+    desc: 'The operation was successful',
+  },
+  error: {
+    bgColor: 'red',
+    icon: '',
+    title: 'Alert',
+    desc: 'The operation has failed',
+  },
+  info: {
+    bgColor: 'blue',
+    icon: '',
+    title: 'Info',
+    desc: 'You have some info to check',
+  },
+  warn: {
+    bgColor: 'yellow',
+    icon: '',
+    title: 'Warning',
+    desc: 'There are some problems',
+  },
+}
