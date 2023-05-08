2461f5035c95e6338687de892a55fa16447e1cd1
Jamal <jamalrahimzadegan@gmail.com>
2023-05-08 17:45:38 +0100

    Update toggle.tsx

--- src/toggle/toggle.tsx
+++ src/toggle/toggle.tsx
@@ -1,26 +1,39 @@
-import React, { ChangeEvent } from 'react'
+import React, { InputHTMLAttributes, useState } from 'react'
 import { Wrapper } from './style'
 
-interface Props {
-  onToggle: (e: ChangeEvent<HTMLInputElement>) => any
+interface Props extends InputHTMLAttributes<any> {
+  onToggle: (isActive: boolean) => any
   label: string
   enabled: boolean
   height?: number
   width?: number
+  className?: string
 }
 
 export default function Toggle(props: Props): JSX.Element {
-  const { label, enabled, onToggle, height = 20, width = 40 } = props
+  const {
+    label,
+    enabled = false,
+    onToggle,
+    height = 17,
+    width = 34,
+    className = '',
+    ...restProps
+  } = props
+  const [isActive, setIsActive] = useState<boolean>(enabled)
+
+  const handleToggle = () => {
+    setIsActive(!isActive)
+    onToggle?.(isActive)
+  }
 
   return (
-    <Wrapper height={height} width={width}>
-      <input
-        type='checkbox'
-        defaultChecked={enabled}
-        onChange={(e) => onToggle?.(e)}
-      />
-      <span />
+    <button onClick={handleToggle} className={className}>
+      <Wrapper height={height} width={width}>
+        <input type='checkbox' checked={isActive} readOnly {...restProps} />
+        <span className='toggle-container' />
+      </Wrapper>
       <strong>{label}</strong>
-    </Wrapper>
+    </button>
   )
 }
