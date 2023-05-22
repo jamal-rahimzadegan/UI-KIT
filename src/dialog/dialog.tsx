22ce07d79d9b3e2b61f428b44082d450147cc735
Jamal <jamalrahimzadegan@gmail.com>
2023-05-22 17:56:03 +0100

    Update dialog style

--- src/dialog/dialog.tsx
+++ src/dialog/dialog.tsx
@@ -1,56 +1,9 @@
-import React, { ReactNode } from 'react'
-import D from './style'
+import styled from 'styled-components'
 
-interface Props {
-  cb?: Function
-  className?: string
-  children: ReactNode
-  title: string
-  desc: string
-}
-
-function UI(props: Props): JSX.Element {
-  const { children, className = '' } = props
-
-  return (
-    <D.Container className={className} onClick={manager.closeOnBackdropClick}>
-      <>{children}</>
-    </D.Container>
-  )
-}
-
-class DialogManager {
-  get dialog(): HTMLDialogElement {
-    return document.querySelector('dialog') as HTMLDialogElement
+const Container = styled.dialog`
+  ::backdrop {
+    background-color: rgba(0, 0, 0, 0.4);
   }
+`
 
-  show() {
-    this.dialog.showModal()
-  }
-
-  close() {
-    this.dialog.close()
-  }
-
-  closeOnBackdropClick = () => {
-    const dialog = this.dialog
-
-    dialog!.addEventListener('click', (e: any) => {
-      const dialogDimensions = dialog!.getBoundingClientRect()
-      const isClickedOutsideOfDialog =
-        e.clientX < dialogDimensions.left ||
-        e.clientX > dialogDimensions.right ||
-        e.clientY < dialogDimensions.top ||
-        e.clientY > dialogDimensions.bottom
-
-      if (isClickedOutsideOfDialog) dialog!.close()
-    })
-  }
-}
-
-const manager = new DialogManager()
-
-export default {
-  UI,
-  manager,
-}
+export default { Container }
