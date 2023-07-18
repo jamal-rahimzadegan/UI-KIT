d6d5737df34d2b7360eef03ccbcb2b0b36eda71e
Jamal <jamalrahimzadegan@gmail.com>
2023-07-18 18:00:20 +0100

    Update style.ts

--- src/dialog/style.ts
+++ src/dialog/style.ts
@@ -1,6 +1,13 @@
 import styled from 'styled-components'
 
 const Container = styled.dialog`
+  top: 50%;
+  left: 50%;
+  -webkit-transform: translateX(-50%) translateY(-50%);
+  -moz-transform: translateX(-50%) translateY(-50%);
+  -ms-transform: translateX(-50%) translateY(-50%);
+  transform: translateX(-50%) translateY(-50%);
+  
   &::backdrop {
     background-color: rgba(0, 0, 0, 0.3);
     backdrop-filter: blur(2px);
