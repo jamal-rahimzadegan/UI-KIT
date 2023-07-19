348eba95a33173ba5154b71c9a3a1befd2ce8453
Jamal <jamalrahimzadegan@gmail.com>
2023-07-19 14:31:07 +0100

    Update dialog.tsx

--- src/dialog/dialog.tsx
+++ src/dialog/dialog.tsx
@@ -1,18 +1,32 @@
-import React, { ReactNode } from 'react'
-import manager from './manager'
-import D from './style'
+import { MouseEvent, PropsWithChildren, useEffect, useRef } from 'react';
+import D from './style';
 
-interface Props {
-  cb?: Function
-  className?: string
-  children: ReactNode
+interface Props extends PropsWithChildren {
+  isOpen: boolean;
+  onClose: () => void;
 }
 
-function DialogUI(props: Props): JSX.Element {
-  return <D.Container {...props} onClick={manager.closeOnBackdropClick} />
-}
+export default function Dialog(props: Props) {
+  const { isOpen, onClose, children } = props;
+  const dialogRef = useRef<HTMLDialogElement>(null);
 
-export default {
-  UI: DialogUI,
-  manager,
+  const isClickInsideRectangle = (e: MouseEvent, element: HTMLElement) => {
+    const r = element.getBoundingClientRect();
+    return e.clientX > r.left && e.clientX < r.right && e.clientY > r.top && e.clientY < r.bottom;
+  };
+
+  const handleClick = (e: any) => {
+    return dialogRef.current && !isClickInsideRectangle(e, dialogRef.current) && onClose();
+  };
+
+  useEffect(() => {
+    if (isOpen) dialogRef.current?.showModal();
+    else dialogRef.current?.close();
+  }, [isOpen]);
+
+  return (
+    <D.Container ref={dialogRef} onCancel={onClose} onClick={handleClick}>
+      {children}
+    </D.Container>
+  );
 }
