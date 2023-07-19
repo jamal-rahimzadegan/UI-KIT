ac2125166c1aa403e355ad79e68e64d1c44f0b62
Jamal <jamalrahimzadegan@gmail.com>
2023-07-19 14:41:33 +0100

    Update dialog.tsx

--- src/dialog/dialog.tsx
+++ src/dialog/dialog.tsx
@@ -10,18 +10,17 @@ export default function Dialog(props: Props) {
   const { isOpen, onClose, children } = props;
   const dialogRef = useRef<HTMLDialogElement>(null);
 
-  const isClickInsideRectangle = (e: MouseEvent, element: HTMLElement) => {
+  const checkIsClickedInside = (e: MouseEvent, element: HTMLElement) => {
     const r = element.getBoundingClientRect();
     return e.clientX > r.left && e.clientX < r.right && e.clientY > r.top && e.clientY < r.bottom;
   };
 
   const handleClick = (e: any) => {
-    return dialogRef.current && !isClickInsideRectangle(e, dialogRef.current) && onClose();
+    return dialogRef.current && !checkIsClickedInside(e, dialogRef.current) && onClose();
   };
 
   useEffect(() => {
-    if (isOpen) dialogRef.current?.showModal();
-    else dialogRef.current?.close();
+    isOpen ? dialogRef.current?.showModal() : dialogRef.current?.close();
   }, [isOpen]);
 
   return (
