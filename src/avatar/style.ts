ccf630aafcf682efafc8afa06bd9938a9732b7ff
Jamal <jamalrahimzadegan@gmail.com>
2023-08-25 11:20:38 +0100

    Update style.ts

--- src/avatar/style.ts
+++ src/avatar/style.ts
@@ -1,25 +1,39 @@
 import styled from "styled-components";
 
-const AvatarContainer = styled.div<{ hasImg: boolean }>`
-  height: 40px;
-  width: 40px;
-  border-radius: 40px;
+const Container = styled.div<{ hasImg: boolean; size: number }>`
+  height: ${({ size }) => size}px;
+  width: ${({ size }) => size}px;
+  border-radius: ${({ size }) => size}px;
   display: flex;
   align-items: center;
   justify-content: center;
   overflow: hidden;
   box-shadow: 0 0 1rem rgba(0, 0, 0, 0.1);
-  background: ${({hasImg}) => (hasImg ? "white" : "transparent")};
+  background: ${({ hasImg }) => (hasImg ? "inherit" : "transparent")};
+  box-shadow: 0px 0px 5px 0px rgba(0, 0, 0, 0.4);
+  font-size: 14px;
+  font-weight: bold;
+
+  img {
+    height: 100%;
+    width: 100%;
+    object-fit: cover;
+    transform: scale(1.1);
+    transform-origin: top center;
+
+    &[alt]: after {
+      display: block;
+      position: absolute;
+      top: 0;
+      left: 0;
+      width: 100%;
+      height: 100%;
+      background-color: #fff;
+      line-height: ${({ size }) => size / 16};
+      text-align: center;
+      content: attr(alt);
+    }
+  }
 `;
 
-const UserNameTitle = styled.div`
-	font-weight: bold;
-`;
-
-const UserImg = styled.img`
-	height: 100%;
-	width: 100%;
-	object-fit: cover;
-`;
-
-export { AvatarContainer, UserNameTitle, UserImg };
+export default { Container };
