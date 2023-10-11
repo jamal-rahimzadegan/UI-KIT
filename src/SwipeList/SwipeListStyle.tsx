affd4bd50534f5c2209f568990021cd198a76eb0
Jamal <jamalrahimzadegan@gmail.com>
2023-10-11 11:27:53 +0100

    Update  style of SwipteList

--- src/SwipeList/SwipeListStyle.tsx
+++
@@ -1,6 +0,0 @@
-import styled, { css } from 'styled-components';
-import { FONT_SIZES } from 'constant';
-
-export const SwipeListContainer = styled.div`
-  // font-weight: ${({ isBold }) => (isBold ? 'bolder' : 'unset')};
-`;
---
+++ src/swipte-list/style.tsx
@@ -0,0 +1,4 @@
+import styled, { css } from 'styled-components';
+
+export const Container = styled.div`
+`;
