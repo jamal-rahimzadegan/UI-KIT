7461b91feb3a2484f315c72ebd1f7da84c8b2f5d
Jamal <jamalrahimzadegan@gmail.com>
2023-10-11 12:16:31 +0100

    Update swipe-list.tsx

--- src/swipte-list/swipe-list.tsx
+++ src/swipte-list/swipe-list.tsx
@@ -1,31 +1,20 @@
 import React, { useState, TouchEvent } from 'react';
 
+interface Props {
+  children: ReactNode[]
+  showBullets?: boolean
+}
+
+type Direction = 'right' | 'left'
+
 function SwipeList(props: Props) {
-  const { children } = props
+  const { children, showBullets = true } = props
   const [activeTab, setActiveTab] = useState<number>(0)
   const LAST_INDEX: number = children?.length - 1
   const THRESHOLD: number = 75
   let touchStartPos: number = 0
   let touchStopPos: number = 0
-
-  const handleSwipe = () => {
-    let index = 0
-    let shouldResetIndex = false
-
-    // Swiped Left
-    if (touchStartPos - touchStopPos > THRESHOLD) {
-      shouldResetIndex = activeTab === 0
-      index = shouldResetIndex ? LAST_INDEX : activeTab - 1
-    }
-
-    // Swiped Right
-    if (touchStartPos - touchStopPos < -THRESHOLD) {
-      shouldResetIndex = activeTab === LAST_INDEX
-      index = shouldResetIndex ? 0 : activeTab + 1
-    }
-
-    setActiveTab(index)
-  }
+  const hasTouchScreen = 'ontouchstart' in document.documentElement
 
   const handleTouch = (e: TouchEvent<HTMLDivElement>) => {
     switch (e.type) {
@@ -40,18 +29,49 @@ function SwipeList(props: Props) {
     }
   }
 
+  const handleSwipe = (dir?: Direction) => {
+    let index = 0
+    let shouldResetIndex = false
+
+    // Swiped Left
+    if (dir === 'left' || touchStartPos - touchStopPos > THRESHOLD) {
+      shouldResetIndex = activeTab === 0
+      index = shouldResetIndex ? LAST_INDEX : activeTab - 1
+    }
+
+    // Swiped Right
+    else if (dir === 'right' || touchStartPos - touchStopPos < -THRESHOLD) {
+      shouldResetIndex = activeTab === LAST_INDEX
+      index = shouldResetIndex ? 0 : activeTab + 1
+    }
+
+    setActiveTab(index)
+  }
+
   return (
-    <div
-      onTouchStart={handleTouch}
-      onTouchMove={handleTouch}
-      onTouchEnd={handleTouch}
-      style={{
-        display: 'flex',
-        alignItems: 'center',
-        justifyContent: 'center',
-      }}
-    >
-      {children[activeTab]}
-    </div>
+    <>
+      <Container
+        onTouchStart={handleTouch}
+        onTouchMove={handleTouch}
+        onTouchEnd={handleTouch}
+      >
+        {hasTouchScreen ? null : <Arrow dir='left' swipe={handleSwipe} />}
+        <ContentContainer>{children[activeTab]}</ContentContainer>
+        {hasTouchScreen ? null : <Arrow dir='right' swipe={handleSwipe} />}
+      </Container>
+      {showBullets ? (
+        <BulletContainer>
+          {children.map((item, i) => (
+            <BulletItem key={i} isActive={i === activeTab} />
+          ))}
+        </BulletContainer>
+      ) : null}
+    </>
   )
 }
+
+const Arrow = ({ dir, swipe }: { dir: Direction; swipe: Function }) => (
+  <ArrowBtn dir={dir} onClick={() => swipe(dir)}>
+    <p>{dir === 'left' ? '<' : '>'}</p>
+  </ArrowBtn>
+)
