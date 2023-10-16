50cd6b703f8ab2de9f420d7dd3ee88eb2c2195be
Jamal <jamalrahimzadegan@gmail.com>
2023-10-16 11:49:52 +0100

    Delete src/HyperLink directory

--- src/HyperLink/HyperLinkStyle.tsx
+++
@@ -1,6 +0,0 @@
-import styled from 'styled-components';
-
-export const A = styled.a`
-  color: ${({ theme, color }) => theme[color || 'PRIMARY_TEXT']} !important;
-  background-color: ${({ theme, bgColor }) => theme[bgColor || 'TRANSPARENT']} !important;
-`;
