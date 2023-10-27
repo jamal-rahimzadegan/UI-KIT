af4c207629a7cec31d0e4dfdd30578f19c6e14c8
Jamal <jamalrahimzadegan@gmail.com>
2023-10-27 10:02:54 +0100

    Create meter.tsx

---
+++ src/meter/meter.tsx
@@ -0,0 +1,16 @@
+interface Props {
+  label: string
+  min?: number
+  max: number
+  value: number
+}
+
+export default function Meter(props: Props) {
+  const { label, min = 0, max, value } = props
+  return (
+    <Container>
+      <label>{label}</label>
+      <meter min={min} max={max} value={value} />
+    </Container>
+  )
+}
